35cb2254485b72eb9506ddc909d5a996b533912d
Ali <ali1195shah@gmail.com>
2019-10-01 11:31:23 -0400

    prompt created

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,15 @@
 require_relative '../config/environment'
 
-puts "hello world"
+interface = Interface.new()
+user = interface.welcome()
+
+while user.nil?
+    user = interface.welcome()
+end
+
+User.validation(user)
+
+
+
+binding.pry
+0
