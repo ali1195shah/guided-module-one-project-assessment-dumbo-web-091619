a85f0542bd527b357ded848872f89449275e8c66
Ali <ali1195shah@gmail.com>
2019-10-01 17:39:03 -0400

    ali&emirhan %57 completed

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "elephant_id"
     t.integer "price"
     t.string "title"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- lib/app/listing.rb
+++ lib/app/listing.rb
@@ -7,22 +7,45 @@ class Listing < ActiveRecord::Base
     def self.main_menu(user)
         puts "--- Main Menu ---"
         @@prompt.select("What would you like to do? 🐘") do |menu|
-            menu.choice "🐘  Check Balance", -> { "This is your balance: $#{user.balance}" }
-            menu.choice "🐘  My Elephant(s)", -> { user.my_elephant }
-            menu.choice "🐘  Listings", -> { Listing.display_listing_section }
-            menu.choice "🐘  Order History", -> { Listing.order_history }
-            menu.choice "🐘  Account Settings", -> { User.account_settings }
+            menu.choice "🐘  Check My Balance", -> { user.show_my_balance(user) }
+            menu.choice "🐘  See My Elephant(s)", -> { user.my_elephant(user) }
+            menu.choice "🐘  See All Listings That Are Available", -> { Listing.display_all_listings(user) }
+            menu.choice "🐘  See My Order History", -> { Listing.order_history }
+            menu.choice "🐘  See my Account Settings", -> { User.account_settings }
         end
     end
 
-    
+    def self.display_all_listings(user)
+        title_arr = Listing.all.map(&:title)
+        chosen_title = @@prompt.select("Here are all the Titles for Listings: ", title_arr)
 
-    def self.display_listing_section
-        
+        listing1 = Listing.all.find{|listing| listing.title == chosen_title}
+        elephant_id = listing1.elephant_id
+        elephant_obj = Elephant.all.find{|elephant| elephant.id == elephant_id}
+
+            puts "Name: #{elephant_obj.name}"
+            puts "From: #{elephant_obj.affiliation}"
+            puts "Date of Birth: #{elephant_obj.dob}"
+            puts "Species: #{elephant_obj.species}"
+            puts "Gender: #{elephant_obj.gender}"
+            puts "About: #{elephant_obj.note}"
+            puts "Bought for: $#{elephant_obj.worth}"
+
+        @@prompt.select("<--------->") do |sub|
+            sub.choice "Buy This Elephant", -> { user.buy_elephant(listing1)}
+            sub.choice "Go Back to Main Menu...", -> {Listing.main_menu(user)}
+        end
     end
 
-    def self.order_history
-        
+    def order_history
+        if order_history.size == 0
+            puts "You don't have any orders 😭"
+        else
+            puts "Here are your past orders: "
+            # First we shoud do the buy method.
+        end
     end
 
 end
+
+
